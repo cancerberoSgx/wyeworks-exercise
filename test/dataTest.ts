d5749e204228120e1efaa1078e0ea91feb89301b
Sebastián Gurín <sebastigurin@gmail.com>
2019-11-10 11:49:09 -0300

    test

--- test/dataTest.ts
+++ test/dataTest.ts
@@ -3,5 +3,5 @@ import { parse } from '../src/data'
 
 test('parse', async t=>{
   const data = parse('test/assets/discography.txt')
-  t.deepEqual(data.length, 10)
+  t.deepEqual(data.length, 47)
 })
